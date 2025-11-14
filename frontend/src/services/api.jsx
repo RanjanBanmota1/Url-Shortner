35a771e14e1075cdf964815ba5f1066cf0dd15e6
RanjanBanmota1 <Ranjanbanmotanitj@gmail.com>
2025-11-14 23:53:39 +0530

    api resolved

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://url-shortner-8l3w.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
